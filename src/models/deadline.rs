218429b99ecb33543f2a14b66b711b35fce87eac
Sergei Nikitin <kexa76@gmail.com>
2025-10-16 12:31:16 +0300

    wip

--- src/models/deadline.rs
+++ src/models/deadline.rs
@@ -35,3 +35,46 @@ impl Deadline {
         }
     }
 }
+
+#[derive(Clone, Default, Debug, PartialEq, Serialize, Deserialize)]
+pub struct UpdateDeadline {
+    /// Timestamp дэдлайна
+    #[serde(rename = "deadline", skip_serializing_if = "Option::is_none")]
+    pub deadline: Option<f64>,
+    /// Timestamp начала задачи
+    #[serde(rename = "startDate", skip_serializing_if = "Option::is_none")]
+    pub start_date: Option<f64>,
+    /// Отображать на стикере время, или только дату
+    #[serde(rename = "withTime", skip_serializing_if = "Option::is_none")]
+    pub with_time: Option<bool>,
+    /// История изменений дедлайна
+    #[serde(rename = "history", skip_serializing_if = "Option::is_none")]
+    pub history: Option<Vec<String>>,
+    /// Точки, которые блокируют дату дедлайна (Начало или Конец)
+    #[serde(rename = "blockedPoints")]
+    pub blocked_points: Vec<String>,
+    /// Связанные задачи
+    #[serde(rename = "links")]
+    pub links: Vec<String>,
+    /// Открепить стикер от задачи (true)
+    #[serde(rename = "deleted", skip_serializing_if = "Option::is_none")]
+    pub deleted: Option<bool>,
+    /// Прикрепить стикер дедлайна без значения
+    #[serde(rename = "empty", skip_serializing_if = "Option::is_none")]
+    pub empty: Option<bool>,
+}
+
+impl UpdateDeadline {
+    pub fn new(blocked_points: Vec<String>, links: Vec<String>) -> UpdateDeadline {
+        UpdateDeadline {
+            deadline: None,
+            start_date: None,
+            with_time: None,
+            history: None,
+            blocked_points,
+            links,
+            deleted: None,
+            empty: None,
+        }
+    }
+}
