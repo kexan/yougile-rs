784a24bf25059381c65ebf53c1d63b679ca7dbcd
Sergei Nikitin <kexa76@gmail.com>
2025-10-16 15:52:14 +0300

    wip

--- src/apis/users.rs
+++ src/apis/users.rs
@@ -1,311 +1,123 @@
-use super::{configuration, ContentType, Error};
-use crate::{apis::ResponseContent, models};
-use reqwest;
-use serde::{de::Error as _, Deserialize, Serialize};
+use crate::{
+    apis::{
+        configuration::Configuration,
+        parse_response, RequestBuilderExt,
+    },
+    models::{CreateUser, User, UserList, UpdateUser},
+    YougileError,
+};
 
-/// struct for typed errors of method [`user_controller_create`]
-#[derive(Debug, Clone, Serialize, Deserialize)]
-#[serde(untagged)]
-pub enum UserControllerCreateError {
-    UnknownValue(serde_json::Value),
-}
+const USERS_PATH: &str = "/api-v2/users";
 
-/// struct for typed errors of method [`user_controller_delete`]
-#[derive(Debug, Clone, Serialize, Deserialize)]
-#[serde(untagged)]
-pub enum UserControllerDeleteError {
-    Status404(),
-    UnknownValue(serde_json::Value),
-}
+pub async fn create_user(
+    configuration: &Configuration,
+    create_user: CreateUser,
+) -> Result<crate::models::Id, YougileError> {
+    let url = format!("{}{}", configuration.base_path, USERS_PATH);
 
-/// struct for typed errors of method [`user_controller_get`]
-#[derive(Debug, Clone, Serialize, Deserialize)]
-#[serde(untagged)]
-pub enum UserControllerGetError {
-    Status404(),
-    UnknownValue(serde_json::Value),
-}
-
-/// struct for typed errors of method [`user_controller_search`]
-#[derive(Debug, Clone, Serialize, Deserialize)]
-#[serde(untagged)]
-pub enum UserControllerSearchError {
-    Status404(),
-    UnknownValue(serde_json::Value),
-}
-
-/// struct for typed errors of method [`user_controller_update`]
-#[derive(Debug, Clone, Serialize, Deserialize)]
-#[serde(untagged)]
-pub enum UserControllerUpdateError {
-    Status404(),
-    UnknownValue(serde_json::Value),
-}
-
-pub async fn user_controller_create(
-    configuration: &configuration::Configuration,
-    create_user_dto: models::CreateUser,
-) -> Result<models::Id, Error<UserControllerCreateError>> {
-    // add a prefix to parameters to efficiently prevent name collisions
-    let p_body_create_user_dto = create_user_dto;
-
-    let uri_str = format!("{}/api-v2/users", configuration.base_path);
-    let mut req_builder = configuration
+    let resp = configuration
         .client
-        .request(reqwest::Method::POST, &uri_str);
+        .post(&url)
+        .json(&create_user)
+        .with_auth_headers(configuration)
+        .send()
+        .await?;
 
-    if let Some(ref user_agent) = configuration.user_agent {
-        req_builder = req_builder.header(reqwest::header::USER_AGENT, user_agent.clone());
-    }
-    if let Some(ref token) = configuration.bearer_access_token {
-        req_builder = req_builder.bearer_auth(token.to_owned());
-    };
-    req_builder = req_builder.json(&p_body_create_user_dto);
-
-    let req = req_builder.build()?;
-    let resp = configuration.client.execute(req).await?;
-
-    let status = resp.status();
-    let content_type = resp
-        .headers()
-        .get("content-type")
-        .and_then(|v| v.to_str().ok())
-        .unwrap_or("application/octet-stream");
-    let content_type = super::ContentType::from(content_type);
-
-    if !status.is_client_error() && !status.is_server_error() {
-        let content = resp.text().await?;
-        match content_type {
-            ContentType::Json => serde_json::from_str(&content).map_err(Error::from),
-            ContentType::Text => Err(Error::from(serde_json::Error::custom("Received `text/plain` content type response that cannot be converted to `models::WithIdDto`"))),
-            ContentType::Unsupported(unknown_type) => Err(Error::from(serde_json::Error::custom(format!("Received `{unknown_type}` content type response that cannot be converted to `models::WithIdDto`")))),
-        }
-    } else {
-        let content = resp.text().await?;
-        let entity: Option<UserControllerCreateError> = serde_json::from_str(&content).ok();
-        Err(Error::ResponseError(ResponseContent {
-            status,
-            content,
-            entity,
-        }))
-    }
+    parse_response(resp).await
 }
 
-pub async fn user_controller_delete(
-    configuration: &configuration::Configuration,
+pub async fn delete_user(
+    configuration: &Configuration,
     id: &str,
-) -> Result<models::Id, Error<UserControllerDeleteError>> {
-    // add a prefix to parameters to efficiently prevent name collisions
-    let p_path_id = id;
-
-    let uri_str = format!(
-        "{}/api-v2/users/{id}",
-        configuration.base_path,
-        id = crate::apis::urlencode(p_path_id)
+) -> Result<crate::models::Id, YougileError> {
+    let encoded_id = crate::apis::urlencode(id);
+    let url = format!(
+        "{}{}/{}",
+        configuration.base_path, USERS_PATH, encoded_id
     );
-    let mut req_builder = configuration
+
+    let resp = configuration
         .client
-        .request(reqwest::Method::DELETE, &uri_str);
+        .delete(&url)
+        .with_auth_headers(configuration)
+        .send()
+        .await?;
 
-    if let Some(ref user_agent) = configuration.user_agent {
-        req_builder = req_builder.header(reqwest::header::USER_AGENT, user_agent.clone());
-    }
-    if let Some(ref token) = configuration.bearer_access_token {
-        req_builder = req_builder.bearer_auth(token.to_owned());
-    };
-
-    let req = req_builder.build()?;
-    let resp = configuration.client.execute(req).await?;
-
-    let status = resp.status();
-    let content_type = resp
-        .headers()
-        .get("content-type")
-        .and_then(|v| v.to_str().ok())
-        .unwrap_or("application/octet-stream");
-    let content_type = super::ContentType::from(content_type);
-
-    if !status.is_client_error() && !status.is_server_error() {
-        let content = resp.text().await?;
-        match content_type {
-            ContentType::Json => serde_json::from_str(&content).map_err(Error::from),
-            ContentType::Text => Err(Error::from(serde_json::Error::custom("Received `text/plain` content type response that cannot be converted to `models::WithIdDto`"))),
-            ContentType::Unsupported(unknown_type) => Err(Error::from(serde_json::Error::custom(format!("Received `{unknown_type}` content type response that cannot be converted to `models::WithIdDto`")))),
-        }
-    } else {
-        let content = resp.text().await?;
-        let entity: Option<UserControllerDeleteError> = serde_json::from_str(&content).ok();
-        Err(Error::ResponseError(ResponseContent {
-            status,
-            content,
-            entity,
-        }))
-    }
+    parse_response(resp).await
 }
 
-pub async fn user_controller_get(
-    configuration: &configuration::Configuration,
+pub async fn get_user(
+    configuration: &Configuration,
     id: &str,
-) -> Result<models::User, Error<UserControllerGetError>> {
-    // add a prefix to parameters to efficiently prevent name collisions
-    let p_path_id = id;
-
-    let uri_str = format!(
-        "{}/api-v2/users/{id}",
-        configuration.base_path,
-        id = crate::apis::urlencode(p_path_id)
+) -> Result<User, YougileError> {
+    let encoded_id = crate::apis::urlencode(id);
+    let url = format!(
+        "{}{}/{}",
+        configuration.base_path, USERS_PATH, encoded_id
     );
-    let mut req_builder = configuration.client.request(reqwest::Method::GET, &uri_str);
 
-    if let Some(ref user_agent) = configuration.user_agent {
-        req_builder = req_builder.header(reqwest::header::USER_AGENT, user_agent.clone());
-    }
-    if let Some(ref token) = configuration.bearer_access_token {
-        req_builder = req_builder.bearer_auth(token.to_owned());
-    };
+    let resp = configuration
+        .client
+        .get(&url)
+        .with_auth_headers(configuration)
+        .send()
+        .await?;
 
-    let req = req_builder.build()?;
-    let resp = configuration.client.execute(req).await?;
-
-    let status = resp.status();
-    let content_type = resp
-        .headers()
-        .get("content-type")
-        .and_then(|v| v.to_str().ok())
-        .unwrap_or("application/octet-stream");
-    let content_type = super::ContentType::from(content_type);
-
-    if !status.is_client_error() && !status.is_server_error() {
-        let content = resp.text().await?;
-        match content_type {
-            ContentType::Json => serde_json::from_str(&content).map_err(Error::from),
-            ContentType::Text => Err(Error::from(serde_json::Error::custom("Received `text/plain` content type response that cannot be converted to `models::UserDto`"))),
-            ContentType::Unsupported(unknown_type) => Err(Error::from(serde_json::Error::custom(format!("Received `{unknown_type}` content type response that cannot be converted to `models::UserDto`")))),
-        }
-    } else {
-        let content = resp.text().await?;
-        let entity: Option<UserControllerGetError> = serde_json::from_str(&content).ok();
-        Err(Error::ResponseError(ResponseContent {
-            status,
-            content,
-            entity,
-        }))
-    }
+    parse_response(resp).await
 }
 
-pub async fn user_controller_search(
-    configuration: &configuration::Configuration,
+pub async fn search_user(
+    configuration: &Configuration,
     limit: Option<f64>,
     offset: Option<f64>,
     email: Option<&str>,
     project_id: Option<&str>,
-) -> Result<models::UserList, Error<UserControllerSearchError>> {
-    // add a prefix to parameters to efficiently prevent name collisions
-    let p_query_limit = limit;
-    let p_query_offset = offset;
-    let p_query_email = email;
-    let p_query_project_id = project_id;
+) -> Result<UserList, YougileError> {
+    let url = format!("{}{}", configuration.base_path, USERS_PATH);
 
-    let uri_str = format!("{}/api-v2/users", configuration.base_path);
-    let mut req_builder = configuration.client.request(reqwest::Method::GET, &uri_str);
+    let mut query_params = vec![];
+    if let Some(val) = limit {
+        query_params.push(("limit", val.to_string()));
+    }
+    if let Some(val) = offset {
+        query_params.push(("offset", val.to_string()));
+    }
+    if let Some(val) = email {
+        query_params.push(("email", val.to_string()));
+    }
+    if let Some(val) = project_id {
+        query_params.push(("projectId", val.to_string()));
+    }
 
-    if let Some(ref param_value) = p_query_limit {
-        req_builder = req_builder.query(&[("limit", &param_value.to_string())]);
-    }
-    if let Some(ref param_value) = p_query_offset {
-        req_builder = req_builder.query(&[("offset", &param_value.to_string())]);
-    }
-    if let Some(ref param_value) = p_query_email {
-        req_builder = req_builder.query(&[("email", &param_value.to_string())]);
-    }
-    if let Some(ref param_value) = p_query_project_id {
-        req_builder = req_builder.query(&[("projectId", &param_value.to_string())]);
-    }
-    if let Some(ref user_agent) = configuration.user_agent {
-        req_builder = req_builder.header(reqwest::header::USER_AGENT, user_agent.clone());
-    }
-    if let Some(ref token) = configuration.bearer_access_token {
-        req_builder = req_builder.bearer_auth(token.to_owned());
-    };
+    let resp = configuration
+        .client
+        .get(&url)
+        .query(&query_params)
+        .with_auth_headers(configuration)
+        .send()
+        .await?;
 
-    let req = req_builder.build()?;
-    let resp = configuration.client.execute(req).await?;
-
-    let status = resp.status();
-    let content_type = resp
-        .headers()
-        .get("content-type")
-        .and_then(|v| v.to_str().ok())
-        .unwrap_or("application/octet-stream");
-    let content_type = super::ContentType::from(content_type);
-
-    if !status.is_client_error() && !status.is_server_error() {
-        let content = resp.text().await?;
-        match content_type {
-            ContentType::Json => serde_json::from_str(&content).map_err(Error::from),
-            ContentType::Text => Err(Error::from(serde_json::Error::custom("Received `text/plain` content type response that cannot be converted to `models::UserListDto`"))),
-            ContentType::Unsupported(unknown_type) => Err(Error::from(serde_json::Error::custom(format!("Received `{unknown_type}` content type response that cannot be converted to `models::UserListDto`")))),
-        }
-    } else {
-        let content = resp.text().await?;
-        let entity: Option<UserControllerSearchError> = serde_json::from_str(&content).ok();
-        Err(Error::ResponseError(ResponseContent {
-            status,
-            content,
-            entity,
-        }))
-    }
+    parse_response(resp).await
 }
 
-pub async fn user_controller_update(
-    configuration: &configuration::Configuration,
+pub async fn update_user(
+    configuration: &Configuration,
     id: &str,
-    update_user_dto: models::UpdateUser,
-) -> Result<models::Id, Error<UserControllerUpdateError>> {
-    // add a prefix to parameters to efficiently prevent name collisions
-    let p_path_id = id;
-    let p_body_update_user_dto = update_user_dto;
-
-    let uri_str = format!(
-        "{}/api-v2/users/{id}",
-        configuration.base_path,
-        id = crate::apis::urlencode(p_path_id)
+    update_user: UpdateUser,
+) -> Result<crate::models::Id, YougileError> {
+    let encoded_id = crate::apis::urlencode(id);
+    let url = format!(
+        "{}{}/{}",
+        configuration.base_path, USERS_PATH, encoded_id
     );
-    let mut req_builder = configuration.client.request(reqwest::Method::PUT, &uri_str);
 
-    if let Some(ref user_agent) = configuration.user_agent {
-        req_builder = req_builder.header(reqwest::header::USER_AGENT, user_agent.clone());
-    }
-    if let Some(ref token) = configuration.bearer_access_token {
-        req_builder = req_builder.bearer_auth(token.to_owned());
-    };
-    req_builder = req_builder.json(&p_body_update_user_dto);
+    let resp = configuration
+        .client
+        .put(&url)
+        .json(&update_user)
+        .with_auth_headers(configuration)
+        .send()
+        .await?;
 
-    let req = req_builder.build()?;
-    let resp = configuration.client.execute(req).await?;
-
-    let status = resp.status();
-    let content_type = resp
-        .headers()
-        .get("content-type")
-        .and_then(|v| v.to_str().ok())
-        .unwrap_or("application/octet-stream");
-    let content_type = super::ContentType::from(content_type);
-
-    if !status.is_client_error() && !status.is_server_error() {
-        let content = resp.text().await?;
-        match content_type {
-            ContentType::Json => serde_json::from_str(&content).map_err(Error::from),
-            ContentType::Text => Err(Error::from(serde_json::Error::custom("Received `text/plain` content type response that cannot be converted to `models::WithIdDto`"))),
-            ContentType::Unsupported(unknown_type) => Err(Error::from(serde_json::Error::custom(format!("Received `{unknown_type}` content type response that cannot be converted to `models::WithIdDto`")))),
-        }
-    } else {
-        let content = resp.text().await?;
-        let entity: Option<UserControllerUpdateError> = serde_json::from_str(&content).ok();
-        Err(Error::ResponseError(ResponseContent {
-            status,
-            content,
-            entity,
-        }))
-    }
+    parse_response(resp).await
 }
